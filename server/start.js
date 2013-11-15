067106b545c19239ca58d2792a83b26f36196d08
Ritchie Martori <skawful@gmail.com>
2013-11-15 13:39:22 -0800

    PORT and Procfile fixes for heroku

--- server/start.js
+++ server/start.js
@@ -1,4 +1,4 @@
-var PORT = process.argv[2] && parseInt(process.argv[2], 10) || 3000;
+var PORT = process.env.PORT || process.argv[2] && parseInt(process.argv[2], 10) || 3000;
 var STATIC_DIR = __dirname + '/../app';
 var TEST_DIR = __dirname + '/../test';
 var DATA_FILE = __dirname + '/data/restaurants.json';
